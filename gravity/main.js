21f34ee7f75ba4281616ba6f02aa17b38942177b
stlenx <69406526+stlenx@users.noreply.github.com>
2021-04-02 22:19:57 +0200

    i guess i did things

--- gravity/main.js
+++ gravity/main.js
@@ -1,15 +1,23 @@
 const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
-let sun = {
+let planet = {
     x: 500,
     y: 500,
     mass: 500000,
     vx: 0,
-    vy: 0
+    vy: 0.5
 }
 
-let planet = {
+let planet2 = {
+    x: 500,
+    y: 700,
+    mass: 500000,
+    vx: 0,
+    vy: 0.5
+}
+
+let sun = {
     x: 400,
     y: 500,
     mass: 1000,
@@ -17,36 +25,43 @@ let planet = {
     vy: -5
 }
 
+let planets = []
+
+planets.push(planet);
+//planets.push(planet2);
+planets.push(sun);
+
 function renderObjects() {
     ctx.clearRect(0, 0, 1000, 1000);
+    Array.prototype.forEach.call(planets, function (planet) {
+        Array.prototype.forEach.call(planets, function (planet2) {
+            planet = addGravity(planet, planet2)
+        })
 
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(sun.x, sun.y, 10, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(planet.x, planet.y, 5, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(planet.x, planet.y, 5, 0, Math.PI*2, true);
+        ctx.closePath();
+        ctx.fill();
+    })
 
     updateObjects()
-
-    planet = addGravity(planet, sun)
-    //sun = addGravity(sun, planet)
-
     //console.log(planet.x,planet.y)
 
     window.requestAnimationFrame(renderObjects);
 }
 
 function updateObjects() {
-    planet.x += planet.vx;
-    planet.y += planet.vy;
-    sun.x += sun.vx;
-    sun.y += sun.vy;
+    Array.prototype.forEach.call(planets, function (planet) {
+        planet = addGravity(planet, planet2)
+        planet.x += planet.vx;
+        planet.y += planet.vy;
+
+        if(planet.y > 1000) planet.y = 0
+        if(planet.y < 0) planet.y = 1000
+        if(planet.x > 1000) planet.x = 0
+        if(planet.x < 0) planet.x = 1000
+    })
 }
 
 function addGravity(a,b) {
@@ -74,8 +89,7 @@ function getGravitationalForce(a,b) {
 }
 
 function getDistanceBetween(a, b) {
-    return Math.sqrt( ((b.x - a.x) * (b.x - a.x)) + ((b.y - a.y) * b.y - a.y) )
+    return Math.sqrt( ((b.x - a.x) * (b.x - a.x)) + ((b.y - a.y) * (b.y - a.y)) )
 }
 
 window.requestAnimationFrame(renderObjects);
-
--- signs/signs.js
+++ signs/signs.js
@@ -7,96 +7,93 @@ Http.onreadystatechange = () => {
     if (Http.readyState !== 4 || Http.status !== 200) return; // Check for ready because xmlhttprequest gae
 
     let output = JSON.parse(Http.responseText)
-
     console.log(output)
 
     if (output.pageResults !== undefined) {
-
-        let video = document.getElementById('video');
-        let source = document.getElementById('source');
-
-        source.setAttribute("src", output.pageResults.videoURL)
-
-        video.load();
-
-        video.loop = true;
-
-
-        //Variations time
-        let container = document.getElementById("variations");
-
-        while (container.hasChildNodes()) {
-            container.removeChild(container.lastChild);
-        }
-
-        let pos = 0;
-        Array.prototype.forEach.call(output.pageResults.variations, function(i){
-
-            //Make the radio button
-            let input = document.createElement("input");
-            input.id = i.type;
-            input.name = "variations";
-            input.value = i.url;
-            input.onclick = function() { getSign(this.value); };
-            input.type = "radio";
-
-
-            //Check what sign variation we have selected and check it
-            if(i.url.slice(i.url.length - 1) === loadedSign.slice(loadedSign.length - 1)){
-                input.checked = true;
-            }
-
-            //Make the label for the button
-            let tag = document.createElement("label")
-            tag.setAttribute("for", i.type)
-            tag.innerHTML = i.type;
-
-
-            //Add the new elements
-            container.appendChild(input)
-            container.appendChild(tag)
-
-            pos++;
-        });
-
-    } else {
-
-        let container = document.getElementById("container");
-
-        while (container.hasChildNodes()) {
-            container.removeChild(container.lastChild);
-        }
-
-        let pos = 0;
-        Array.prototype.forEach.call(output.searchResults.results, function(i){
-
-            //Make the radio button
-            let input = document.createElement("input");
-            input.id = i.context.slice(7, i.context.length -1);
-            input.name = "interpretations";
-            input.value = i.pageLink;
-            input.onclick = function() { getSign(this.value); };
-            input.type = "radio";
-
-            //If it's the first button, mark it as checked and get the video
-            if(pos === 0) {
-                input.checked = true;
-                getSign(input.value);
-            }
-
-            //Make the label for the button
-            let tag = document.createElement("label")
-            tag.setAttribute("for", i.context.slice(7, i.context.length -1))
-            tag.innerHTML = i.context.slice(7, i.context.length -1);
-
-
-            //Add the new elements
-            container.appendChild(input)
-            container.appendChild(tag)
-
-            pos++;
-        });
+        getVideo(output)
+        return
     }
+
+    getInterpretations(output)
+}
+
+function getInterpretations(output) {
+    let container = document.getElementById("container");
+
+    while (container.hasChildNodes()) container.removeChild(container.lastChild)
+
+    let pos = 0;
+    Array.prototype.forEach.call(output.searchResults.results, function(i){
+
+        //Make the radio button
+        let input = makeRadioInput(
+            i.context.slice(7, i.context.length -1),
+            "interpretations",
+            i.pageLink,
+            function() { getSign(this.value); })
+
+        //If it's the first button, mark it as checked and get the video
+        if(pos === 0) {
+            input.checked = true;
+            getSign(input.value);
+        }
+
+        //Make the label for the button
+        let tag = document.createElement("label")
+        tag.setAttribute("for", i.context.slice(7, i.context.length -1))
+        tag.innerHTML = i.context.slice(7, i.context.length -1);
+
+
+        //Add the new elements
+        container.appendChild(input)
+        container.appendChild(tag)
+
+        pos++;
+    });
+}
+
+function getVideo(output) {
+    document.getElementById('source').setAttribute("src", output.pageResults.videoURL)
+    document.getElementById('video').load()
+
+    //Variations time
+    let container = document.getElementById("variations");
+
+    while (container.hasChildNodes()) container.removeChild(container.lastChild)
+
+    let pos = 0;
+    Array.prototype.forEach.call(output.pageResults.variations, function(i){
+        //Make the radio button
+        let input = makeRadioInput(i.type,"variations", i.url, function() { getSign(this.value); })
+
+        //Check what sign variation we have selected and check it
+        if(i.url.slice(i.url.length - 1) === loadedSign.slice(loadedSign.length - 1))
+            input.checked = true;
+
+        //Make the label for the button
+        let tag = document.createElement("label")
+        tag.setAttribute("for", i.type)
+        tag.innerHTML = i.type;
+
+
+        //Add the new elements
+        container.appendChild(input)
+        container.appendChild(tag)
+
+        pos++;
+    });
+}
+
+function makeRadioInput(id, name, value, onclick) {
+    let input = document.createElement("input")
+
+    input.id = id;
+    input.name = name;
+    input.value = value;
+    input.onclick = onclick;
+    input.type = "radio";
+
+    return input;
 }
 
 function inputChanged(input) {
