b90108d43222891eb850ba7bf47d7561ab42de69
stlenx <69406526+stlenx@users.noreply.github.com>
2021-04-06 23:33:56 +0200

    FLUID DYNAMICS ARE SO COOL

--- fluids/Fluid.js
+++ fluids/Fluid.js
@@ -1,48 +1,31 @@
 function IX(x,y) {
-    return x+ (y * N);
+    return x + y * N;
 }
 
 class Fluid {
-    size
-    dt
-    diff
-    visc
-
-    s = []
-    density = Array(4096)
-
-    Vx = []
-    Vy = []
-
-    Vx0 = []
-    Vy0 = []
-
-    constructor(dt, diffusion, viscosity, N) {
-        this.size = N;
+    constructor(dt, diffusion, viscosity) {
         this.dt = dt;
         this.diff = diffusion;
         this.visc = viscosity;
-    }
 
-    initializeArrays() {
-        console.log(this.density.length)
-        for (let i = 0; i < N; i++) {
-            for (let j = 0; j < N; j++) {
-                this.density.push(1)
-            }
-        }
-        console.log(this.density)
+        this.s = new Array(N * N).fill(0);
+        this.density = new Array(N * N).fill(0);
+
+        this.Vx = new Array(N * N).fill(0);
+        this.Vy = new Array(N * N).fill(0);
+
+        this.Vx0 = new Array(N * N).fill(0);
+        this.Vy0 = new Array(N * N).fill(0);
     }
 
     addDensity(x, y, amount) {
         let index = IX(x,y)
         this.density[index] += amount;
-        console.log("added density")
     }
 
     //REMEMBER THIS USES A VECTOR2 FOR AMOUNT!!
     addVelocity(x, y, amount) {
-        let index = IX(x,y)
+        let index = IX(x, y);
         this.Vx[index] += amount.x;
         this.Vy[index] += amount.y;
     }
@@ -54,6 +37,8 @@ class Fluid {
                 let y = j * scale;
                 let d = this.density[IX(i, j)];
 
+                d = d > 255 ? 255 : d;
+
                 ctx.fillStyle = 'rgb(' + d + ',' + d + ',' + d + ')';
                 ctx.beginPath();
                 ctx.rect(x, y, scale, scale);
@@ -61,10 +46,10 @@ class Fluid {
                 ctx.fill();
             }
         }
-        //console.log(this.density)
     }
 
     step() {
+
         diffuse(1, this.Vx0, this.Vx, this.visc, this.dt);
         diffuse(2, this.Vy0, this.Vy, this.visc, this.dt);
 
@@ -83,102 +68,117 @@ class Fluid {
 
 function diffuse(b, x, x0, diff, dt) {
     let a = dt * diff * (N - 2) * (N - 2);
-    lin_solve(b, x, x0, a, 1 + 6 * a, iter, N);
+    lin_solve(b, x, x0, a, 1 + 6 * a);
 }
 
 function lin_solve(b, x, x0, a, c) {
     let cRecip = 1.0 / c;
-    for (let k = 0; k < iter; k++) {
-        for (let j = 1; j < N -1; j++) {
-            for (let i = 1; i < N -1; i++) {
+    for (let t = 0; t < iter; t++) {
+        for (let j = 1; j < N - 1; j++) {
+            for (let i = 1; i < N - 1; i++) {
                 x[IX(i, j)] =
-                    (x0[IX(i, j)] + a * (x[IX(i+1, j)] + x[IX(i-1, j)] + x[IX(i, j+1)] + x[IX(i, j-1)])) * cRecip;
+                    (x0[IX(i, j)] +
+                        a *
+                        (x[IX(i + 1, j)] +
+                            x[IX(i - 1, j)] +
+                            x[IX(i, j + 1)] +
+                            x[IX(i, j - 1)])) *
+                    cRecip;
             }
         }
-        set_bnd(b, x)
+        set_bnd(b, x);
     }
 }
 
 function project(velocX, velocY, p, div) {
-    for (let j = 1; j < N -1; j++) {
-        for (let i = 1; i < N -1; i++) {
-            div[IX(i, j)] = -0.5 * (velocX[IX(i+1, j)] - velocX[IX(i-1,j)] + velocY[IX(i, j+1)] - velocY[IX(i, j-1)]) / N;
+    for (let j = 1; j < N - 1; j++) {
+        for (let i = 1; i < N - 1; i++) {
+            div[IX(i, j)] =
+                (-0.5 *
+                    (velocX[IX(i + 1, j)] -
+                        velocX[IX(i - 1, j)] +
+                        velocY[IX(i, j + 1)] -
+                        velocY[IX(i, j - 1)])) /
+                N;
             p[IX(i, j)] = 0;
         }
     }
 
-    set_bnd(0, div)
-    set_bnd(0, p)
-    lin_solve(0,p,div,1,6)
+    set_bnd(0, div);
+    set_bnd(0, p);
+    lin_solve(0, p, div, 1, 6);
 
-    for (let j = 1; j < N -1; j++) {
-        for (let i = 1; i < N -1; i++) {
-            velocX[IX(i, j)] -= 0.5 * (p[IX(i+1, j)] - p[IX(i-1, j)]) * N;
-            velocY[IX(i, j)] -= 0.5 * (p[IX(i, j+1)] - p[IX(i, j-1)]) * N;
+    for (let j = 1; j < N - 1; j++) {
+        for (let i = 1; i < N - 1; i++) {
+            velocX[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) * N;
+            velocY[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) * N;
         }
     }
 
-    set_bnd(1, velocX)
-    set_bnd(1, velocY)
+    set_bnd(1, velocX);
+    set_bnd(2, velocY);
 }
 
 function advect(b, d, d0, velocX, velocY, dt) {
     let i0, i1, j0, j1;
+
     let dtx = dt * (N - 2);
     let dty = dt * (N - 2);
 
     let s0, s1, t0, t1;
-    let tmp1, tmp2, x, y;
+    let tmp1, tmp2, tmp3, x, y;
 
-    let i, j;
+    let Nfloat = N - 2;
+    let ifloat, jfloat;
+    let i, j, k;
 
-    for (j = 1; j < N - 1; j++) {
-        for(i = 1; i < N - 1; i++) {
+    for (j = 1, jfloat = 1; j < N - 1; j++, jfloat++) {
+        for (i = 1, ifloat = 1; i < N - 1; i++, ifloat++) {
             tmp1 = dtx * velocX[IX(i, j)];
             tmp2 = dty * velocY[IX(i, j)];
-            x = i - tmp1;
-            y = i - tmp2;
+            x = ifloat - tmp1;
+            y = jfloat - tmp2;
 
-            if(x < 0.5) x = 0.5;
-            if(x > N + 0.5) x = N + 0.5;
-            i0 = Math.floor(x)
-            i1 = i0 + 1;
-            if(y < 0.5) y = 0.5;
-            if(y > N + 0.5) y = N + 0.5;
+            if (x < 0.5) x = 0.5;
+            if (x > Nfloat + 0.5) x = Nfloat + 0.5;
+            i0 = Math.floor(x);
+            i1 = i0 + 1.0;
+            if (y < 0.5) y = 0.5;
+            if (y > Nfloat + 0.5) y = Nfloat + 0.5;
             j0 = Math.floor(y);
-            j1 = j0 + 1;
+            j1 = j0 + 1.0;
 
             s1 = x - i0;
-            s0 = 1 - s1;
+            s0 = 1.0 - s1;
             t1 = y - j0;
-            t0 = 1 - t1;
+            t0 = 1.0 - t1;
 
-            let i0i = parseInt(i0)
-            let i1i = parseInt(i1)
-            let j0i = parseInt(j0)
-            let j1i = parseInt(j1)
+            let i0i = parseInt(i0);
+            let i1i = parseInt(i1);
+            let j0i = parseInt(j0);
+            let j1i = parseInt(j1);
 
-            //DOUBLE CHECK!!
-            d[IX(i, j)] = s0 * (t0 * d0[IX(i0i, j0i)] + t1 * d0[IX(i0i, j1i)]) + s1 * (t0 * d0[IX(i1i, j0i)] + t1 * d0[IX(i1i, j1i)])
+            d[IX(i, j)] =
+                s0 * (t0 * d0[IX(i0i, j0i)] + t1 * d0[IX(i0i, j1i)]) +
+                s1 * (t0 * d0[IX(i1i, j0i)] + t1 * d0[IX(i1i, j1i)]);
         }
     }
 
-    set_bnd(b, d)
+    set_bnd(b, d);
 }
 
 function set_bnd(b, x) {
     for (let i = 1; i < N - 1; i++) {
-        x[IX(i,0)] = b === 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
-        x[IX(i,N-1)] = b === 2 ? -x[IX(i, N-1)] : x[IX(i, N-1)];
+        x[IX(i, 0)] = b === 2 ? -x[IX(i, 1)] : x[IX(i, 1)];
+        x[IX(i, N - 1)] = b === 2 ? -x[IX(i, N - 2)] : x[IX(i, N - 2)];
     }
-
     for (let j = 1; j < N - 1; j++) {
-        x[IX(0, j)] = b === 2 ? -x[IX(1, j)] : x[IX(1, j)];
-        x[IX(N-1, j)] = b === 2 ? -x[IX(N-1, j)] : x[IX(N-1, j)];
+        x[IX(0, j)] = b === 1 ? -x[IX(1, j)] : x[IX(1, j)];
+        x[IX(N - 1, j)] = b === 1 ? -x[IX(N - 2, j)] : x[IX(N - 2, j)];
     }
 
-    x[IX(0,0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
-    x[IX(0, N-1)] = 0.5 * (x[IX(1, N-1)] + x[IX(0, N -2)]);
-    x[IX(N-1, 0)] = 0.5 * (x[IX(N-2, 0)] + x[IX(N-1, 1)]);
-    x[IX(N-1, N-1)] = 0.5 * (x[IX(N-2, N-1)] + x[IX(N-1, N-2)]);
+    x[IX(0, 0)] = 0.5 * (x[IX(1, 0)] + x[IX(0, 1)]);
+    x[IX(0, N - 1)] = 0.5 * (x[IX(1, N - 1)] + x[IX(0, N - 2)]);
+    x[IX(N - 1, 0)] = 0.5 * (x[IX(N - 2, 0)] + x[IX(N - 1, 1)]);
+    x[IX(N - 1, N - 1)] = 0.5 * (x[IX(N - 2, N - 1)] + x[IX(N - 1, N - 2)]);
 }
--- fluids/main.js
+++ fluids/main.js
@@ -1,19 +1,37 @@
 const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
-const N = 64;
+const N = 150;
 const iter = 5;
 const scale = 10;
 
 let mPressed = false;
-let fluid = new Fluid(0.1, 0, 0, N)
-//fluid.initializeArrays()
+let fluid = new Fluid(0.1, 0, 0.0000001)
 
 canvas.setAttribute('width', N * scale);
 canvas.setAttribute('height', N * scale);
 
+let cx = parseInt(0.5 * N)
+let cy = parseInt(0.5 * N)
+
 function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(mPressed) {
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                fluid.addDensity(cx + i, cy + j, getRandom(50, 150));
+            }
+        }
+    }
+
+    for (let i = 0; i < 2; i++) {
+        let angle = getRandom(0, 6);
+        let v = new Vector2(Math.cos(angle), Math.sin(angle))
+        v.x *= 4;
+        v.y *= 4;
+
+        fluid.addVelocity(cx, cy, v);
+    }
+
     fluid.step()
 
     fluid.renderD()
@@ -21,9 +39,12 @@ function render() {
     window.requestAnimationFrame(render)
 }
 
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 canvas.addEventListener('mousedown', function (e) {
     mPressed = true;
-    console.log(e.offsetX / scale, e.offsetY / scale)
 })
 
 canvas.addEventListener('mouseup', function (e) {
@@ -31,9 +52,13 @@ canvas.addEventListener('mouseup', function (e) {
 })
 
 canvas.addEventListener('mousemove', function (e) {
-    if(mPressed) {
-        fluid.addDensity(Math.floor(e.offsetX / scale), Math.floor(e.offsetY / scale), 100)
-    }
+    //if(mPressed) {
+    //    fluid.addDensity(e.offsetX / scale, e.offsetY / scale, 10)
+    //    fluid.addVelocity(e.offsetX / scale, e.offsetY / scale, new Vector2(Math.cos(1.3), Math.sin(1.3)));
+    //}
+
+    cx = parseInt(e.offsetX / scale);
+    cy = parseInt(e.offsetY / scale);
 })
 
 window.requestAnimationFrame(render)
