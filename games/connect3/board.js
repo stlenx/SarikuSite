3703824c92a301c0c1b72f4e5152b3b97805f982
stlenx <69406526+stlenx@users.noreply.github.com>
2021-04-30 19:14:16 +0200

    what the fuck is this lolz

--- games/connect3/board.js
+++ games/connect3/board.js
@@ -1,28 +1,155 @@
 class Board {
-    constructor(w, h) {
+    constructor(w, h, turn, depth) {
         this.w = w;
         this.h = h;
+        this.b = new Array(3)
+        this.winner = null;
+        this.turn = turn;
+        this.depth = depth--;
+        if(depth - 1 > 0) {
+            let Nturn = turn === 1 ? 2 : 1;
+            this.VB = new Board(w,h, Nturn, depth--)
+        }
+
+        for (let x = 0; x < 3; x++) {
+            this.b[x] = new Array(3);
+            for (let y = 0; y < 3; y++) {
+                this.b[x][y] = 0;
+            }
+        }
+        console.log(this.b)
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0,0,this.w,this.h)
+
+        //Draw lines
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 5;
+        ctx.beginPath()
+        let size = this.w / 3;
+        for (let i=1; i < 3; i++) {
+            ctx.moveTo(size * i,0)
+            ctx.lineTo(size * i,this.h)
+            ctx.moveTo(0,size * i)
+            ctx.lineTo(this.w,size * i)
+        }
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+    AiTurn() {
+        if(this.winner !== null) return;
+        if(this.VB === undefined) return;
+
+
+        this.VB.turn = this.turn === 1 ? 2 : 1;
+
+        let moves = this.Evaluate()
+
+        let move = moves.points.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+        console.log(moves.moves, move, this.depth)
+        console.log(moves.points)
+
+        this.b[moves.moves[move].x][moves.moves[move].y] = this.turn;
+        this.turn = this.turn === 1 ? 2 : 1;
+    }
+
+    Evaluate() {
+        let moves = [];
+        let points = [];
+
+        for(let x = 0; x < 3; x++) {
+            for(let y = 0; y < 3; y++) {
+
+                if(this.b[x][y] === 0 && this.VB !== undefined) {
+                    for(let x2 = 0; x2 < 3; x2++) {
+                        for(let y2 = 0; y2 < 3; y2++) {
+                            this.VB.b[x2][y2] = this.b[x2][y2]
+                        }
+                    }
+
+                    this.VB.b[x][y] = this.turn;
+                    this.VB.AiTurn()
+                    this.VB.Update()
+
+                    switch (this.VB.winner) {
+                        case this.turn:
+                            moves.push({x, y})
+                            points.push(9999)
+                            break;
+                        case this.VB.turn:
+                            moves.push({x, y})
+                            points.push(-9999)
+                            break;
+                        default:
+                            moves.push({x, y})
+                            points.push(0)
+                            break;
+                    }
+                }
+            }
+        }
+        return {moves, points};
+    }
+
+    Update() {
+        let tru = 0;
+
+        //Check vertically
+        for (let x = 0; x < 3; x++) {
+            if(this.b[x][0] !== 0 && this.b[x][0] === this.b[x][1] && this.b[x][0] === this.b[x][2]) {
+                tru = this.b[x][0]
+            }
+        }
+
+        //Check horizontally
+        for (let y = 0; y < 3; y++) {
+            if(this.b[0][y] !== 0 && this.b[0][y] === this.b[1][y] && this.b[0][y] === this.b[2][y]) {
+                tru = this.b[0][y]
+            }
+        }
+
+        //Check diagonal
+        if(this.b[0][0] !== 0 && this.b[0][0] === this.b[1][1] && this.b[0][0] === this.b[2][2]) tru = this.b[0][0];
+        if(this.b[2][0] !== 0 && this.b[2][0] === this.b[1][1] && this.b[2][0] === this.b[0][2]) tru = this.b[2][0];
+
+        if(tru !== 0) this.winner = tru;
+    }
+
+    Win() {
+        switch (this.winner) {
+            case 1:
+                ctx.fillStyle = "GREEN"
+                ctx.font = `100px Helvetica`;
+                ctx.fillText("◯ player WINNSS", this.w / 2, this.h / 2)
+                break;
+            case 2:
+                ctx.fillStyle = "GREEN"
+                ctx.font = `100px Helvetica`;
+                ctx.fillText("✕ player WINNSS", this.w / 2, this.h / 2)
+                break;
+        }
     }
 
     Draw() {
-        //Draw background
-        Bctx.fillStyle = "black";
-        Bctx.fillRect(0,0,this.w,this.h);
-
-        //Draw lines
-        Bctx.strokeStyle = "white";
-        Bctx.lineWidth = 5;
-        Bctx.beginPath()
-        let size = this.w / 3;
-        for (let i=1; i < 3; i++) {
-            Bctx.moveTo(size * i,0)
-            Bctx.lineTo(size * i,this.h)
-            Bctx.moveTo(0,size * i)
-            Bctx.lineTo(this.w,size * i)
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle"
+        ctx.font = `300px Helvetica`;
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                switch (this.b[x][y]) {
+                    case 1:
+                        ctx.fillText("◯", (this.w / 3) * x + (this.w / 3) / 2, ((this.h / 3) * y + (this.w / 3) / 2) + 300 * 0.066);
+                        break;
+                    case 2:
+                        ctx.fillText("✕", (this.w / 3) * x + (this.w / 3) / 2, ((this.h / 3) * y + (this.w / 3) / 2) + 300 * 0.066);
+                        break;
+                    case 0:
+                        break;
+                }
+            }
         }
-        Bctx.stroke();
-        Bctx.closePath()
-
-
     }
 }
--- games/connect3/main.js
+++ games/connect3/main.js
@@ -1,156 +1,46 @@
-//let canvas = document.getElementById("canvas");
-//canvas.setAttribute('width', window.innerHeight)
-//canvas.setAttribute('height', window.innerHeight)
-//let ctx = canvas.getContext('2d')
-//
-//let board = new Board(canvas.width, canvas.height)
-//
-//function frame() {
-//    Draw()
-//
-//    window.requestAnimationFrame(frame)
-//}
-//
-//function Draw() {
-//    ctx.clearRect(0,0,canvas.width, canvas.height)
-//
-//    board.Draw()
-//}
-//
-//window.requestAnimationFrame(frame)
+let canvas = document.getElementById("canvas");
+canvas.setAttribute('width', window.innerHeight)
+canvas.setAttribute('height', window.innerHeight)
+let ctx = canvas.getContext('2d')
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let particleArray = [];
-let particleSize = 10;
+let board = new Board(canvas.width, canvas.height, 1, 10)
 
-// get mouse mouse position //
-let mouse = {
-    x: null,
-    y: null,
-    radius: 150
-}
-
-window.addEventListener('mousemove', (e) => {
-        mouse.x = e.offsetX
-        mouse.y = e.offsetY;
-});
-
-ctx.font = 'bold 16px Verdana';
-var gradient = ctx.createLinearGradient(0, 0, 70, 0);
-gradient.addColorStop("0", "magenta");
-gradient.addColorStop("1", "blue");
-// Fill with gradient
-ctx.fillStyle = gradient;
-ctx.fillText('SARIKU', 5, 30);
-const data = ctx.getImageData(0, 0, canvas.width, 100);
-
-class Particle {
-    constructor(x, y, color){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.size = particleSize;
-        this.baseX = this.x;
-        this.baseY = this.y;
-        this.density = ((Math.random() * 60) + 1);
+canvas.addEventListener("mousedown", (e) => {
+    let posX = 0;
+    let posY = 0;
+    switch (true) {
+        case e.offsetX < canvas.width / 3:
+            posX = 0;
+            break;
+        case e.offsetX < (canvas.width / 3) * 2:
+            posX = 1;
+            break;
+        case e.offsetX < (canvas.width / 3) * 3:
+            posX = 2;
+            break;
     }
 
-    Draw() {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
+    switch (true) {
+        case e.offsetY < canvas.height / 3:
+            posY = 0;
+            break;
+        case e.offsetY < (canvas.height / 3) * 2:
+            posY = 1;
+            break;
+        case e.offsetY < (canvas.height / 3) * 3:
+            posY = 2;
+            break;
     }
 
-    Update() {
-        // check mouse position/particle position - collision detection
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let distance = Math.sqrt(dx*dx + dy*dy);
-        let forceDirectionX = dx / distance;
-        let forceDirectionY = dy / distance;
-        // distance past which the force is zero
-        let maxDistance = mouse.radius;
-        let force = (maxDistance - distance) / maxDistance;
-
-        // if we went below zero, set it to zero.
-        force = force < 0 ? 0 : force;
-
-        let directionX = (forceDirectionX * force * this.density)
-        let directionY = (forceDirectionY * force * this.density);
-
-        if (distance < mouse.radius + this.size){
-            this.x -= directionX;
-            this.y -= directionY;
-        } else {
-            if (this.x !== this.baseX ) {
-                let dx = this.x - this.baseX;
-                this.x -= dx/10;
-            } if (this.y !== this.baseY) {
-                let dy = this.y - this.baseY;
-                this.y -= dy/10;
-            }
-        }
+    if(board.b[posX][posY] === 0 && board.winner === null) {
+        board.b[posX][posY] = board.turn;
+        board.turn = board.turn === 1 ? 2 : 1;
+        board.Draw()
+        board.Update()
+        if(board.winner !== null) board.Win()
+        board.AiTurn()
+        board.Draw()
+        board.Update()
+        if(board.winner !== null) board.Win()
     }
-}
-
-function init(){
-    for (let y = 0; y < data.height; y++) {
-        for (let x = 0; x < data.width; x++) {
-            if (data.data[((x + y * data.width) * 4) + 3] > 128) {
-                let index = (x + y * data.width) * 4;
-                let R = data.data[index];
-                let G = data.data[index + 1];
-                let B = data.data[index + 2];
-                let color = "rgb(" + R + "," + G + "," + B + ")";
-                particleArray.push(new Particle(x * 15, y * 15, color));
-            }
-        }
-    }
-}
-
-function connect() {
-    for (let a = 0; a < particleArray.length; a++) {
-        for (let b = a; b < particleArray.length; b++) {
-            let distance = ((particleArray[a].x - particleArray[b].x) * (particleArray[a].x - particleArray[b].x))
-                + ((particleArray[a].y - particleArray[b].y) * (particleArray[a].y - particleArray[b].y));
-            if (distance < 3600) {
-                let opacityValue = 1 - (distance / 3600);
-                let dx = mouse.x - particleArray[a].x;
-                let dy = mouse.y - particleArray[a].y;
-                let mouseDistance = Math.sqrt(dx * dx + dy * dy);
-                if (mouseDistance < mouse.radius) {
-                    particleArray[a].size = Remap(mouseDistance, 0, mouse.radius, 30, particleSize);
-                } else {
-                    particleArray[a].size = particleSize;
-                }
-
-                ctx.strokeStyle = 'rgba(255,255,255,' + opacityValue + ')';
-                ctx.lineWidth = 2;
-                ctx.beginPath();
-                ctx.moveTo(particleArray[a].x, particleArray[a].y);
-                ctx.lineTo(particleArray[b].x, particleArray[b].y);
-                ctx.stroke();
-            }
-        }
-    }
-}
-
-function animate(){
-    ctx.clearRect(0,0,innerWidth,innerHeight);
-
-    connect();
-
-    particleArray.forEach((particle) => {
-        particle.Update()
-        particle.Draw()
-    })
-
-    requestAnimationFrame(animate);
-}
-init();
-animate();
+})
