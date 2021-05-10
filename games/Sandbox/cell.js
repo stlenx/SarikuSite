1cce550a918911bcfd83063b3259b0027925d523
stlenx <69406526+stlenx@users.noreply.github.com>
2021-05-10 19:43:03 +0200

    The lofi hip hop radio beats to relax studi to girl is girl maker is watching owo AAAAA help
    yes. Pay attention to the guy above

--- games/Sandbox/cell.js
+++ games/Sandbox/cell.js
@@ -1,5 +1,34 @@
 class Cell {
-    constructor(type) {
-        this.type = type;
+    constructor(x, y, Gtype) {
+        this.x = x;
+        this.y = y;
+        this.type = Gtype;
+        switch (Gtype) {
+            case type.empty:
+                this.r = 0;
+                this.g = 0;
+                this.b = 0;
+                break;
+            case type.sand:
+                this.r = 76;
+                this.g = 70;
+                this.b = 50;
+                break;
+            case type.water:
+                this.r = 78;
+                this.g = 141;
+                this.b = 200;
+                break;
+        }
+    }
+
+    Update() {
+        if(this.y < canvas.height - 10) {
+            if(world[this.x][this.y+1].type === type.empty) {
+                this.y = this.y+1;
+                world[this.x][this.y] = this;
+                world[this.x][this.y -1] = new Cell(this.x, this.y -1, type.empty)
+            }
+        }
     }
 }
--- games/Sandbox/main.js
+++ games/Sandbox/main.js
@@ -2,42 +2,142 @@ let canvas = document.getElementById("canvas");
 canvas.setAttribute("width", window.innerWidth)
 canvas.setAttribute("height", window.innerHeight)
 let ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+const type = {
+    empty: "empty",
+    sand: "sand",
+    water: "water"
+}
 
 let world = [];
+let mouse = {
+    clicked: false,
+    radius: 10,
+    x: 0,
+    y:0,
+    type: type.water
+}
 
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+const imageData = ctx.createImageData(500, 500);
+const data = imageData.data;
 //Initialize world
-for(let x = 0; x < canvas.width; x++) {
-    world[x] = new Array(canvas.height)
-    for(let y = 0; y < canvas.height; y++) {
-        world[x][y] = new Cell();
+for(let x = 0; x < imageData.width; x++) {
+    world[x] = new Array(imageData.height)
+    for(let y = 0; y < imageData.height; y++) {
+        world[x][y] = new Cell(x, y, type.empty);
     }
 }
 
-console.log(world)
+let newCanvas = document.createElement("canvas")
+newCanvas.setAttribute("width", imageData.width)
+newCanvas.setAttribute("height", imageData.height)
+let newCtx = newCanvas.getContext("2d")
 
-function DrawImage(values) {
-    for (let x = 0; x < size; x++) {
-        for (let y = 0; y < size; y++) {
-            let n = values[x][y];
-            let index = (y + x * size) * 4;
-            if (n === -1)
-            {
-                //writeColor( x, y, 0, 0, 0, 255);
-                data[index] = 0;
-                data[index+1] = 0;
-                data[index+2] = 0;
-            }
-            else
-            {
-                let hueValue = remap(0,iterations, 0,1, n);
-                let color = hslToRgb(hueValue, 0.8,0.5)
-                //console.log(color, hueValue)
-                data[index] = color[1];
-                data[index+1] = color[2];
-                data[index+2] = color[3];
-            }
+function DrawWorld() {
+    for (let x = 0; x < world.length; x++) {
+        for (let y = 0; y < world[x].length; y++) {
+            let c = world[x][y];
+            let index = (y * imageData.width + x) * 4;
+
+            data[index] = c.r;
+            data[index+1] = c.g;
+            data[index+2] = c.b;
             data[index + 3] = 255;
         }
     }
-    ctx.putImageData(imageData, 0, 0);
+    newCtx.putImageData(imageData, 0,0)
+    ctx.drawImage(newCanvas,0,0, window.innerHeight, window.innerHeight)
+    //ctx.putImageData(imageData, 0, 0);
 }
+
+function UpdateWorld() {
+    let newWorld = new Array(world.length);
+    for(let x = 0; x < world.length; x++) {
+        newWorld[x] = new Array(world[x].length)
+        for(let y = 0; y < world[x].length; y++) {
+            newWorld[x][y] = new Cell(x,y, world[x][y].type);
+        }
+    }
+
+    for(let x = 1; x < imageData.width - 1; x++) {
+        for(let y = 0; y < imageData.height - 1; y++) {
+            switch (world[x][y].type) {
+                case type.sand:
+                    if(world[x][y+1].type === type.empty) {
+                        newWorld[x][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    } else if(world[x - 1][y + 1].type === type.empty) {
+                        newWorld[x - 1][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    } else if(world[x + 1][y + 1].type === type.empty) {
+                        newWorld[x + 1][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    }
+                    break;
+                case type.water:
+                    if(world[x][y+1].type === type.empty) {
+                        newWorld[x][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    } else if(world[x - 1][y + 1].type === type.empty) {
+                        newWorld[x - 1][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    } else if(world[x + 1][y + 1].type === type.empty) {
+                        newWorld[x + 1][y + 1] = world[x][y];
+                        newWorld[x][y] = new Cell(x, y, type.empty)
+                    } else if(world[x - 1][y].type === type.empty) {
+                        if(newWorld[x-1][y].type === type.empty) {
+                            newWorld[x - 1][y] = world[x][y];
+                            newWorld[x][y] = new Cell(x, y, type.empty)
+                        }
+                    } else if(world[x + 1][y].type === type.empty) {
+                        if(newWorld[x+1][y].type === type.empty) {
+                            newWorld[x + 1][y] = world[x][y];
+                            newWorld[x][y] = new Cell(x, y, type.empty)
+                        }
+                    }
+                    break;
+                case type.empty:
+                    break;
+            }
+        }
+    }
+
+    world = newWorld;
+}
+
+function frame() {
+    DrawWorld()
+
+    UpdateWorld()
+
+
+
+    if(mouse.clicked) {
+        for(let x = mouse.x - mouse.radius; x < mouse.x + mouse.radius; x++) {
+            for(let y = mouse.y - mouse.radius; y < mouse.y + mouse.radius; y++) {
+                world[x][y] = new Cell(x, y, mouse.type)
+            }
+        }
+    }
+
+    window.requestAnimationFrame(frame)
+}
+
+canvas.addEventListener("mousedown", (e) => {
+    mouse.x = Math.floor(Remap(e.offsetX, 0, window.innerHeight, 0, 500));
+    mouse.y = Math.floor(Remap(e.offsetY, 0, window.innerHeight, 0, 500));
+    mouse.clicked = true;
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    mouse.x = Math.floor(Remap(e.offsetX, 0, window.innerHeight, 0, 500));
+    mouse.y = Math.floor(Remap(e.offsetY, 0, window.innerHeight, 0, 500));
+})
+
+canvas.addEventListener("mouseup", (e) => {
+    mouse.clicked = false;
+})
+
+window.requestAnimationFrame(frame)
