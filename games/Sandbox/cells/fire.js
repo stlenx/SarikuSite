848984a9a3507f1471946fa51ba661c07b5f2f7e
stlenx <69406526+stlenx@users.noreply.github.com>
2021-05-13 20:27:42 +0200

    better fire

--- games/Sandbox/cells/fire.js
+++ games/Sandbox/cells/fire.js
@@ -12,6 +12,18 @@ class Fire {
         this.r = color[type.fire][0] + randomC
         this.g = color[type.fire][1] + randomC
         this.b = color[type.fire][2] + randomC
+
+        this.directions = [
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+            new Vector2(-1, 1),
+            new Vector2(1, 0),
+            new Vector2(0, 0),
+            new Vector2(-1, 0),
+            new Vector2(1, -1),
+            new Vector2(0, -1),
+            new Vector2(-1, -1),
+        ]
     }
 
     Update() {
@@ -22,22 +34,42 @@ class Fire {
             newWorld[this.x][this.y] = new Fire(this.x, this.y, this.lifespan, this.isBurning)
         }
 
-        if(world[this.x][this.y+1].flammable) {
-            this.MightBurn(this.x, this.y+1)
-        } else if(world[this.x - 1][this.y + 1].flammable) {
-            this.MightBurn(this.x-1, this.y+1)
-        } else if(world[this.x + 1][this.y + 1].flammable) {
-            this.MightBurn(this.x+1, this.y+1)
-        } else if(world[this.x - 1][this.y].flammable) {
-            this.MightBurn(this.x - 1, this.y)
-        } else if(world[this.x + 1][this.y].flammable) {
-            this.MightBurn(this.x + 1, this.y)
-        } else if(world[this.x][this.y - 1].flammable) {
-            this.MightBurn(this.x, this.y-1)
-        } else if(world[this.x - 1][this.y - 1].flammable) {
-            this.MightBurn(this.x-1, this.y-1)
-        } else if(world[this.x + 1][this.y - 1].flammable) {
-            this.MightBurn(this.x+1, this.y-1)
+        this.CheckDirections(this.directions)
+
+        //if(world[this.x][this.y+1].flammable) {
+        //    this.MightBurn(this.x, this.y+1)
+        //} else if(world[this.x - 1][this.y + 1].flammable) {
+        //    this.MightBurn(this.x-1, this.y+1)
+        //} else if(world[this.x + 1][this.y + 1].flammable) {
+        //    this.MightBurn(this.x+1, this.y+1)
+        //} else if(world[this.x - 1][this.y].flammable) {
+        //    this.MightBurn(this.x - 1, this.y)
+        //} else if(world[this.x + 1][this.y].flammable) {
+        //    this.MightBurn(this.x + 1, this.y)
+        //} else if(world[this.x][this.y - 1].flammable) {
+        //    this.MightBurn(this.x, this.y-1)
+        //} else if(world[this.x - 1][this.y - 1].flammable) {
+        //    this.MightBurn(this.x-1, this.y-1)
+        //} else if(world[this.x + 1][this.y - 1].flammable) {
+        //    this.MightBurn(this.x+1, this.y-1)
+        //}
+    }
+
+    CheckDirections(directions) {
+        if(directions.length > 1) {
+            let pos = Math.floor(getRandom(0, directions.length))
+
+            if(world[this.x + directions[pos].x][this.y + directions[pos].y].flammable) {
+                this.MightBurn(this.x + directions[pos].x, this.y + directions[pos].y)
+            } else {
+                directions.splice(pos, 1)
+
+                this.CheckDirections(directions)
+            }
+        } else {
+            if(world[this.x + directions[0].x][this.y + directions[0].y].flammable) {
+                this.MightBurn(this.x + directions[0].x, this.y + directions[0].y)
+            }
         }
     }
 
