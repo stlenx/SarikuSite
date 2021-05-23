9bc0565f5bc1e54951c4d38c449c0bb5a6de2ba7
stlenx <69406526+stlenx@users.noreply.github.com>
2021-05-23 19:42:14 +0200

    insane

--- math/Raymarching/main.js
+++ math/Raymarching/main.js
@@ -3,54 +3,89 @@ canvas.setAttribute("width", window.innerWidth * 0.8)
 canvas.setAttribute("height", window.innerHeight)
 let ctx = canvas.getContext("2d");
 
+const type = {
+    circle: 0,
+    square: 1
+}
+
+let gpu = new GPU();
 let scene = new Scene(canvas.width, canvas.height,ctx);
-scene.AddElement(new Circle(500,500, 80))
+scene.AddObject(500, 500, type.circle, 50)
+scene.AddObject(500, 500, type.square, 50, 80)
 //scene.AddElement(new Square(600,500, new Vector2(200, 200)))
 //scene.AddElement(new Square(900,500, new Vector2(100, 100)))
 //scene.AddElement(new Circle(450, 500, 80))
-let test = "hi"
 
 function frame() {
 
-    scene.Draw()
     scene.Calculate()
+    scene.Draw()
 
     window.requestAnimationFrame(frame)
 }
 
 canvas.addEventListener("mousemove", (e) => {
-    scene.elements[0].x = e.offsetX;
-    scene.elements[0].y = e.offsetY;
+    scene.objects[0][0] = e.offsetX;
+    scene.objects[0][1] = e.offsetY;
 })
-
-let gpu = new GPU();
-const calc = gpu.createKernel(function (x, y, r) {
-    let dst1 = Math.sqrt( ((x - this.thread.x) * (x - this.thread.x)) + ((y - this.thread.y) * (y - this.thread.y)) )
-    dst1 -= r;
-
-    let dst2 = Math.sqrt( ((500 - this.thread.x) * (500 - this.thread.x)) + ((500 - this.thread.y) * (500 - this.thread.y)) )
-    dst2 -= r;
-
-    let k = 300;
-    let h = Math.max(k-Math.abs(dst1-dst2),0) / k;
-    let result =  Math.min(dst1, dst2) - h * h * h * k/6;
-
-    if(result < 0) {
-        return 255
-    }
-    return 0;
-}).setOutput([canvas.height, canvas.height])
-
-function DumbShitFuck(x, y, r) {
-    return calc(x, y, r);
-}
 
 function AddButton() {
     let element = document.createElement("h2")
-    element.innerHTML = `Element ${scene.elements.length + 1}`
+    element.innerHTML = `Element ${scene.objects.length + 1}`
 
-    scene.AddElement(new Circle(500,500, 80))
-    scene.Calculate()
+    let x = parseInt(document.getElementById("x").value);
+    let y = parseInt(document.getElementById("y").value);
+    scene.AddObject(x, y, type.circle, 50)
+    scene.calc = gpu.createKernel(function (objects, n, k) {
+        if(n < 1) return 0;
+
+        let object1x = objects[0][0];
+        let object1y = objects[0][1];
+        let object1r = objects[0][2];
+        let object1type = objects[0][3];
+
+        let dst = 0;
+
+        if(object1type === 0) {
+            dst = Math.sqrt( ((object1x - this.thread.x) * (object1x - this.thread.x)) + ((object1y - this.thread.y) * (object1y - this.thread.y)) )
+            dst -= object1r;
+        } else {
+            let object1w = objects[0][4];
+            let object1h = objects[0][5];
+            let dx = Math.max(Math.abs(this.thread.x - object1x) - object1w * 0.5, 0);
+            let dy = Math.max(Math.abs(this.thread.y - object1y) - object1h * 0.5, 0);
+            dst = dx * dx + dy * dy;
+            if(dst < 1) dst = -1;
+        }
+
+        for(let i = 1; i < n; i++) {
+            let dstN = 0;
+            let objectNx = objects[i][0];
+            let objectNy = objects[i][1];
+            let objectNr = objects[i][2];
+            let objectNtype = objects[i][3];
+
+            if(objectNtype === 0) {
+                dstN = Math.sqrt( ((objectNx - this.thread.x) * (objectNx - this.thread.x)) + ((objectNy - this.thread.y) * (objectNy - this.thread.y)) )
+                dstN -= objectNr;
+            } else {
+                let objectNw = objects[i][4];
+                let objectNh = objects[i][5];
+                let dx = Math.max(Math.abs(this.thread.x - objectNx) - objectNw * 0.5, 0);
+                let dy = Math.max(Math.abs(this.thread.y - objectNy) - objectNh * 0.5, 0);
+                dstN = dx * dx + dy * dy;
+                if(dstN < 1) dstN = -1;
+            }
+
+            let h = Math.max(k-Math.abs(dst-dstN),0) / k;
+            dst =  Math.min(dst, dstN) - h * h * h * k/6;
+        }
+
+        if(dst < 0) {
+            return 255
+        }
+        return 0;
+    }).setOutput([canvas.height, canvas.height])
 
     document.getElementById("elements").appendChild(element)
 }
--- math/Raymarching/scene.js
+++ math/Raymarching/scene.js
@@ -3,25 +3,82 @@ class Scene {
         this.w = w;
         this.h = h;
         this.ctx = ctx;
-        this.elements = [];
-        this.imageData = null;
+        this.objects = [];
+        this.ImageData = null;
+
+        this.calc = gpu.createKernel(function (objects, n, k) {
+            if(n < 1) return 0;
+
+            let object1x = objects[0][0];
+            let object1y = objects[0][1];
+            let object1r = objects[0][2];
+            let object1type = objects[0][3];
+
+            let dst = 0;
+
+            if(object1type === 0) {
+                dst = Math.sqrt( ((object1x - this.thread.x) * (object1x - this.thread.x)) + ((object1y - this.thread.y) * (object1y - this.thread.y)) )
+                dst -= object1r;
+            } else {
+                let object1w = objects[0][4];
+                let object1h = objects[0][5];
+                let dx = Math.max(Math.abs(this.thread.x - object1x) - object1w * 0.5, 0);
+                let dy = Math.max(Math.abs(this.thread.y - object1y) - object1h * 0.5, 0);
+                dst = dx * dx + dy * dy;
+                if(dst < 1) dst = -1;
+            }
+
+            for(let i = 1; i < n; i++) {
+                let dstN = 0;
+                let objectNx = objects[i][0];
+                let objectNy = objects[i][1];
+                let objectNr = objects[i][2];
+                let objectNtype = objects[i][3];
+
+                if(objectNtype === 0) {
+                    dstN = Math.sqrt( ((objectNx - this.thread.x) * (objectNx - this.thread.x)) + ((objectNy - this.thread.y) * (objectNy - this.thread.y)) )
+                    dstN -= objectNr;
+                } else {
+                    let objectNw = objects[i][4];
+                    let objectNh = objects[i][5];
+                    let dx = Math.max(Math.abs(this.thread.x - objectNx) - objectNw * 0.5, 0);
+                    let dy = Math.max(Math.abs(this.thread.y - objectNy) - objectNh * 0.5, 0);
+                    dstN = dx * dx + dy * dy;
+                    if(dstN < 1) dstN = -1;
+                }
+
+                let h = Math.max(k-Math.abs(dst-dstN),0) / k;
+                dst =  Math.min(dst, dstN) - h * h * h * k/6;
+            }
+
+            if(dst < 0) {
+                return 255
+            }
+            return 0;
+        }).setOutput([h, h])
     }
 
-    AddElement(element) {
-        this.elements.push(element)
+    AddObject(x, y, type, s, sx = 0) {
+        switch (type) {
+            case 0: //Circle
+                this.objects.push([x, y, s, 0, 0, 0])
+                break;
+            case 1:
+                this.objects.push([x, y, 0, 1, s, sx])
+        }
     }
 
     Draw() {
-        if(this.imageData === null) this.Calculate()
-        this.ctx.putImageData(this.imageData, 0,0)
+        if(this.ImageData === null) this.Calculate()
+        this.ctx.putImageData(this.ImageData, 0,0)
     }
 
     Calculate() {
-        this.imageData = this.ctx.createImageData(this.w,this.h);
+        this.ImageData = this.ctx.createImageData(this.w,this.h);
 
-        let output = DumbShitFuck(this.elements[0].x, this.elements[0].y, 50);
+        let output = this.calc(this.objects, this.objects.length, 300);
 
-        let buf = new ArrayBuffer(this.imageData.data.length);
+        let buf = new ArrayBuffer(this.ImageData.data.length);
 
         let buf8 = new Uint8ClampedArray(buf);
         let data = new Uint32Array(buf);
@@ -31,14 +88,14 @@ class Scene {
                 //let col = this.GetPixel(x, y)
                 let col = output[y][x];
 
-                data[y * this.imageData.width + x] =
+                data[y * this.ImageData.width + x] =
                     (255   << 24) |	// alpha
                     (col << 16) |	// blue
                     (col <<  8) |	// green
                     col;		    // red
             }
         }
-        this.imageData.data.set(buf8);
+        this.ImageData.data.set(buf8);
     }
 
     GetPixel(x, y) {
