a17b271964c22e8f9494204f1f36a0a48187dc4b
stlenx <69406526+stlenx@users.noreply.github.com>
2021-06-23 20:31:03 +0200

    aight new thing

--- games/fight/main.js
+++ games/fight/main.js
@@ -1,7 +1,5 @@
 let domain = "fight.sariku.gay";
-
 const Http = new XMLHttpRequest();
-const url=`https://${domain}/games`; //Change to http://fight.sariku.gay/games http://192.168.0.69:6969/games
 
 Http.onreadystatechange = () => {
     if (Http.readyState !== 4 || Http.status !== 200) return; // Check for ready because xmlhttprequest gae
@@ -11,14 +9,23 @@ Http.onreadystatechange = () => {
     console.log(output)
 }
 
+const url=`https://${domain}/games`; //Change to http://fight.sariku.gay/games http://192.168.0.69:6969/games
+Http.open("GET", url);
+Http.send();
+
 function AddRoom(room) {
     let container = document.getElementById("rooms");
 
     //Make container div
     let roomDiv = document.createElement("div");
     roomDiv.setAttribute("class", "room")
-    //roomDiv.innerHTML = room.id;
-    roomDiv.onclick = function () {
+    roomDiv.setAttribute("id", room.id)
+
+
+    //Make info/join button div ----------
+    let joinButton = document.createElement("div")
+    joinButton.setAttribute("class", "joinButton")
+    joinButton.onclick = function () {
         location.href = `game/index.html?room=${room.id}`;
     }
 
@@ -31,11 +38,44 @@ function AddRoom(room) {
     roomPlayers.setAttribute("class", "roomPlayers")
     roomPlayers.innerHTML = `Players: ${room.players}/4`;
 
-    roomDiv.appendChild(roomId)
-    roomDiv.appendChild(roomPlayers)
+    joinButton.appendChild(roomId)
+    joinButton.appendChild(roomPlayers)
+    //End of that lol ------------
+
+
+    //Make close button div -------------
+    let closeIcon = document.createElement("div");
+    closeIcon.setAttribute("class", "closeIcon")
+    closeIcon.onclick = function () {
+        RemoveRoom(room.id)
+    }
+
+    let line1 = document.createElement("div");
+    line1.setAttribute("class", "line");
+    let line2 = document.createElement("div");
+    line2.setAttribute("class", "line2");
+
+    closeIcon.appendChild(line1)
+    closeIcon.appendChild(line2)
+    //End of that lol  ---------
+
+    roomDiv.appendChild(joinButton)
+    roomDiv.appendChild(closeIcon)
 
     container.appendChild(roomDiv)
 }
 
-Http.open("GET", url);
-Http.send();
+function RemoveRoom(id) {
+    let toRemove = document.getElementById(id);
+    toRemove.classList.add("disappear")
+
+    setTimeout(function(){
+        toRemove.remove()
+        Http.open("GET", `https://fight.sariku.gay/games/${id}/delete`);
+        Http.send();
+    }, 500);
+}
+
+function MakeRoomButton() {
+    location.href = `new/index.html`;
+}
